e9b0f88fddc9bcff72a53a4a8318142eee26edd2
JaddM <jaddmiguel@outlook.com>
2024-09-04 18:05:46 -0400

    create git ignore

--- main.py
+++ main.py
@@ -5,42 +5,57 @@ import pyscreenshot as GetImage
 import schedule
 from paddleocr import PaddleOCR, draw_ocr
 from PIL import Image
+import deepl
+
+
+
+OCR = 0
+TRANSLATOR = 0
 
 def triggerScreenshot():
     name = f"image"
     screenshot = GetImage.grab()
     w, h = screenshot.size
-    dimensions = (w-325, h-600, w-35, h-100)
+    dimensions = (w-325, h-600, w-15, h-150)
     cropped = screenshot.crop(dimensions)
     path = f"./screenshots/{name}.png"
     cropped.save(path)
     
     
-
-    ocr = PaddleOCR(lang="en")
-    scraped = ocr.ocr(".\\screenshots\\image.png")
+    scraped = OCR.ocr(".\\screenshots\\image.png")
     for line in scraped[0]:
         parsedText = line[1][0]
         splitText = parsedText.split(":")
+        user = ""
         textToTranslate = ""
         if(len(splitText) == 1):
             textToTranslate = splitText[0]
         else:
             textToTranslate = splitText[1]
-
-
+        print(textToTranslate)
+        if textToTranslate != "":
+            translatedText = TRANSLATOR.translate_text(textToTranslate, target_lang="EN-US")
+        print(translatedText)
+        print("------")
     return path
 
 
+    
+
 def main():
     
+
+    global OCR
+    global TRANSLATOR
+
+    OCR = PaddleOCR(lang="korean")
+    auth=""
+    TRANSLATOR = deepl.Translator(auth)
     schedule.every(5).seconds.do(triggerScreenshot)
 
     while True:
         schedule.run_pending()
         time.sleep(1)
-
-
 
 if __name__ == "__main__":
     main()
